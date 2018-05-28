43f2e71ef6711cd6ffb53afbb50493d2fa562fd0
Unknown <pahonski@gmail.com>
2018-05-28 15:47:47 +0300

    CALCULATOR add parseString method

--- CALCULATOR(B3+)/index.js
+++ CALCULATOR(B3+)/index.js
@@ -4,10 +4,60 @@ button.onclick = function() {
     document.querySelector('.output').innerHTML = `Результат: ${calculate(value)}`;
 }
 
-function calculate(value) {
-    for (let i = 0; i < value.length; i++) {
-        if (value[i] == '(') {
+function parseString(str) {
+    str = str.replace(/\s+/g, '');
 
-        }
+    let part = '';
+    let parts = [];
+    let value = '';
+    let prev = '';
+    
+    for(let i = 0; i < str.length; i++) {
+      value = str[i];
+      console.log(value);
+      switch(value) {
+        case '+':
+        case '*':
+        case '/':
+        case '(':
+        case ')':
+          if(part) {
+            parts.push(part);
+            part = '';
+          }
+          parts.push(value);
+        break;
+        case '-':
+          if(part) {
+            parts.push(part);
+            part = '';
+            parts.push(value);
+          } else {
+            if (      i === 0     ||
+                      prev == '+' ||
+                      prev == '-' ||
+                      prev == '*' ||
+                      prev == '/' ||
+                      prev == '(' 
+          ) {
+              part = value;
+            } else {
+              parts.push(value);
+            }
+          }
+        break;
+        console.log(part);  
+        default: part += value;
+      }
+      prev = value;
     }
-}
+    
+    if(part) {
+      parts.push(part);
+    }
+
+    console.log(parts);
+
+    return parts;
+    
+  }
