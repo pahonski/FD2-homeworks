ad924058340476100257e651f4a29e8d5c17fb4e
Unknown <pahonski@gmail.com>
2018-06-05 20:24:30 +0300

    remove DOM

--- VOWELS_ARR/index.js
+++ VOWELS_ARR/index.js
@@ -1,42 +1,3 @@
-let modeButtons = document.querySelector('.mode');
-let mode = 'foreach';
-modeButtons.onclick = function(e) {
-    let modeElement = e.target;
-    if(modeElement.className == 'modeButton') {
-        mode = modeElement.textContent;
-        let allModeElements = document.querySelectorAll('.modeButton');
-        allModeElements.forEach(function(el) {
-            el.classList.remove('active');
-        })
-        modeElement.classList.add('active');
-        console.log(mode);
-    }
-    
-}
-
-
-
-let button = document.querySelector('.button');
-button.onclick = function() {
-    let value = document.querySelector('.value').value;
-    let output = document.querySelector('.output');
-    switch(mode) {
-        case 'foreach' :
-        output.innerHTML = `Гласных букв: ${getVowelsWithForEach(value)}`;
-        break;
-        case 'filter' :
-        output.innerHTML = `Гласных букв: ${getVowelsWithFilter(value)}`;
-        break;
-        case 'reduce' :
-        output.innerHTML = `Гласных букв: ${getVowelsWithReduce(value)}`;
-        break;
-    }
-    
-}
-
-//аеёиоуыэюя
-
-
 function getVowelsWithForEach(value) {
     let result = 0;
     let vowels = {
@@ -53,41 +14,12 @@ function getVowelsWithForEach(value) {
     }
 
     let valueArray = value.split('');
-    
+
     valueArray.forEach(function(item) {
         if (item in vowels) {
             result += 1;
         }
     });
-
-    let implementation =  document.querySelector('.implementation');
-    implementation.innerHTML = `<pre>
-    let result = 0;
-    let vowels = {
-        а: true,
-        е: true,
-        ё: true,
-        и: true,
-        о: true,
-        у: true,
-        ы: true,
-        э: true,
-        ю: true,
-        я: true
-    }
-
-    let valueArray = value.split('');
-    
-    valueArray.forEach(function(item) {
-        if (item in vowels) {
-            result += 1;
-        }
-    });
-
-    return result;
-
-    </pre>`;
-
 
     return result;
 }
@@ -107,37 +39,10 @@ function getVowelsWithFilter(value) {
     }
 
     let valueArray = value.split('');
-    
+
     let result = valueArray.filter(function(item) {
         return item in vowels;
     });
-
-
-
-    let implementation =  document.querySelector('.implementation');
-    implementation.innerHTML = `<pre>
-    let vowels = {
-        а: true,
-        е: true,
-        ё: true,
-        и: true,
-        о: true,
-        у: true,
-        ы: true,
-        э: true,
-        ю: true,
-        я: true
-    }
-
-    let valueArray = value.split('');
-    
-    let result = valueArray.filter(function(item) {
-        return item in vowels;
-    });
-
-    return result.length;
-    </pre>`;
-
 
     return result.length;
 }
@@ -157,48 +62,19 @@ function getVowelsWithReduce(value) {
     }
 
     let valueArray = value.split('');
-    
+
     let result = valueArray.reduce(function(sum, current) {
-        if(current in vowels) {
+        if (current in vowels) {
             return sum + 1;
         } else {
             return sum + 0;
         }
-    },0);
-
-    console.log(result);
-
-
-
-    let implementation =  document.querySelector('.implementation');
-    implementation.innerHTML = `<pre>
-    let vowels = {
-        а: true,
-        е: true,
-        ё: true,
-        и: true,
-        о: true,
-        у: true,
-        ы: true,
-        э: true,
-        ю: true,
-        я: true
-    }
-
-    let valueArray = value.split('');
-    
-    let result = valueArray.reduce(function(sum, current) {
-        if(current in vowels) {
-            return sum + 1;
-        } else {
-            return sum + 0;
-        }
-    },0);
-
-    return result;
-
-    </pre>`;
-
+    }, 0);
 
     return result;
 }
+
+
+console.log(getVowelsWithForEach('Привет, как дела?'));
+console.log(getVowelsWithFilter('Привет, как дела?'));
+console.log(getVowelsWithReduce('Привет, как дела?'));
