49ef07def485c0057985b5968e7eefa4e804ea34
Unknown <pahonski@gmail.com>
2018-06-06 16:44:02 +0300

    add B5+

---
+++ B5+/index.js
@@ -0,0 +1,29 @@
+function buildWrapper(value) {
+
+  function wrap(text) {
+    let standardMnemonics = {
+      '\'': '&apos;',
+      '\"': '&quot;',
+      '\&': '&amp;',
+      '<': '&lt;',
+      '>': '	&gt;'
+    }
+
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] in standardMnemonics) {
+        text = text.split(text[i]).join(standardMnemonics[text[i]]);
+      }
+    }
+
+
+    let left = `<${value}>`;
+    let right = `</${value}>`;
+    return `${left}${text}${right}`
+  }
+
+  return wrap;
+
+}
+
+let wrapH1 = buildWrapper("H1");
+console.log(wrapH1("Вкусные M&M's"));
