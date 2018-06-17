36869499f9f50b8d575069097a7e4176a15fa0e6
Unknown <pahonski@gmail.com>
2018-06-17 13:49:57 +0300

    ready C3+

--- С3+/index.js
+++ С3+/index.js
@@ -1,55 +1,63 @@
 function deepComp(val1, val2) {
-  let val1Prop = 0;
-  let val2Prop = 0;
+    let val1Prop = 0;
+    let val2Prop = 0;
 
-  val1 = objectSort(val1);
-  val2 = objectSort(val2);
-
- 
-  if (val1 === val2) {
-    return true;
-  }
-
-  if (val1 == null || val2 == null || typeof (val1) != 'object' || typeof (val2) != 'object') {  
-    return false;
-  }
-
-  for (let prop in val1) {
-    val1Prop += 1;
-  }
-
-  for (let prop in val2) {
-    val2Prop += 1;
-    console.log(val1[prop]);
-    if (!(prop in val1) || !deepComp(val1[prop], val2[prop])) {
-      console.log('во дела');
-      return false;
-    }
-  }
-
-
-  function objectSort(obj) {
-    let sortable = [];
-    let result = {};
-
-    for (let item in obj) {
-      sortable.push([item, obj[item]]);
+    if (val1 === null && val2 === undefined) {
+        return false;
     }
 
-    sortable.sort(function (a, b) {
-      if (a[0] != Number && b[0] != Number) {
-        return a[0].charCodeAt(0) - b[0].charCodeAt(0);
-      }
-      return a[0] - b[0];
-    });
+    if (val1 === undefined && val2 === null) {
+        return false;
+    }
 
-    sortable.forEach(item => {
-      result[item[0]] = item[1];
-    });
-    return result;
-  }
+    val1 = (typeof val1 === 'number') ? val1 : objectSort(val1);
+    val2 = (typeof val2 === 'number') ? val2 : objectSort(val2);
 
-return val1Prop == val2Prop;
+    if (val1 === val2) {
+        return true;
+    }
+
+    if ((typeof val1 !== 'object') && (typeof val2 !== 'object') && isNaN(val1) && isNaN(val2)) {
+        return true;
+    }
+
+    if (val1 == null || val2 == null || typeof(val1) != 'object' || typeof(val2) != 'object') {
+        return false;
+    }
+
+    for (let prop in val1) {
+        val1Prop += 1;
+    }
+
+    for (let prop in val2) {
+        val2Prop += 1;
+        if (!(prop in val1) || !deepComp(val1[prop], val2[prop])) {
+            return false;
+        }
+    }
+
+
+    function objectSort(obj) {
+        let sortable = [];
+        let result = {};
+
+        for (let item in obj) {
+            sortable.push([item, obj[item]]);
+        }
+
+        sortable.sort(function(a, b) {
+            if (a[0] != Number && b[0] != Number) {
+                return a[0].charCodeAt(0) - b[0].charCodeAt(0);
+            }
+            return a[0] - b[0];
+        });
+
+        sortable.forEach(item => {
+            result[item[0]] = item[1];
+        });
+        return result;
+    }
+    return val1Prop == val2Prop;
 }
 
 var H1 = { a: 5, b: { b1: 6, b2: 7 } };
@@ -66,22 +74,22 @@ var A1 = [5, 7];
 var A2 = [5, 5, 7];
 var A3 = [5, 8, 7];
 
-console.log(deepComp(6, 66));
+
 // console.log(deepComp({ b1: 6}, { b1: 66}));
 // console.log(deepComp({ b1: 6, b2: 7 }, { b1: 66, b2: 7 }));
 // deepComp(H1, H4);
-// console.log(deepComp(H1,H2))// => true
-// console.log(deepComp(H1,H3))// => false
-// console.log(deepComp(H1,H4))// => false
-// console.log(deepComp(H1,H5))// => false
-// console.log(deepComp(H6,H7))// => true
-// console.log(deepComp(H8,H9))// => false
-// console.log(deepComp(H8,H10))// => false
-// console.log(deepComp(null,H10))// => false
-// console.log(deepComp(H10,null))// => false
-// console.log(deepComp(null,null))// => true
-// console.log(deepComp(null,undefined))// => false
-// console.log(deepComp(5,"5"))// => false
-// console.log(deepComp(5,H1))// => false
-// console.log(deepComp(A1,H1))// => false
-// console.log(deepComp(A2,A3))// => false
+console.log(deepComp(H1, H2)) // => true
+console.log(deepComp(H1, H3)) // => false
+console.log(deepComp(H1, H4)) // => false
+console.log(deepComp(H1, H5)) // => false
+console.log(deepComp(H6, H7)) // => true
+console.log(deepComp(H8, H9)) // => false
+console.log(deepComp(H8, H10)) // => false
+console.log(deepComp(null, H10)) // => false
+console.log(deepComp(H10, null)) // => false
+console.log(deepComp(null, null)) // => true
+console.log(deepComp(null, undefined)) // => false
+console.log(deepComp(5, "5")) // => false
+console.log(deepComp(5, H1)) // => false
+console.log(deepComp(A1, H1)) // => false
+console.log(deepComp(A2, A3)) // => false
