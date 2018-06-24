86a14287b6d2c7c6062d4d7fa7be395b7978e080
Pavel <pahonski@gmail.com>
2018-06-24 17:43:47 +0300

    add DRAGNDROP +

--- DRAGNDROP/index.js
+++ DRAGNDROP/index.js
@@ -1,71 +1,77 @@
 'use strict';
-function dragImages() {
-  let images = document.querySelectorAll('.container img');
-  let container = document.querySelector('.container');
-  let elem;
-  let mouseX, mouseY;
+window.addEventListener('load', function () {
+  function dragImages() {
+    let images = document.querySelectorAll('.container img');
+    let container = document.querySelector('.container');
+    let elem;
+    let mouseX, mouseY;
 
-  renderAbsoluteImg(images);
+    renderAbsoluteImg(images);
 
-  container.addEventListener('mousedown', function (e) {
-    e=e||window.event;
-    e.preventDefault();
 
-    mouseX = e.clientX - e.target.offsetLeft;
-    mouseY = e.clientY - e.target.offsetTop;
+    container.addEventListener('mousedown', function (e) {
+      e=e||window.event;
+      e.preventDefault();
 
-    if(e.target.tagName === 'IMG') {
-      for(let i = 0; i < images.length; i++) {
-        images[i].style.zIndex = 1;
+      mouseX = e.clientX - e.target.offsetLeft;
+      mouseY = e.clientY - e.target.offsetTop;
+
+      if(e.target.tagName === 'IMG') {
+
+        for(let i = 0; i < images.length; i++) {
+          images[i].style.zIndex = 1;
+        }
+        elem = e.target;
+        elem.style.zIndex = 25;
       }
-      elem = e.target;
-      elem.style.zIndex = 25;
+
+    });
+
+    container.addEventListener('mousemove', function (e) {
+      e=e||window.event;
+      e.preventDefault();
+
+      if(elem) {
+        elem.style.left = e.pageX - mouseX + 'px';
+        elem.style.top = e.pageY - mouseY + 'px';
+      }
+
+    });
+
+
+    container.addEventListener('mouseup', function (e) {
+      e=e||window.event;
+      elem = null;
+    });
+
+
+
+    function renderAbsoluteImg(img) {
+
+      for(let i = 0; i < img.length; i++) {
+        let image = img[i];
+        let left = image.offsetLeft;
+        let top = image.offsetTop;
+        image.style.left = left + 'px';
+        image.style.top = top + 'px';
+
+        image.addEventListener('mousemove', function (e) {
+          e=e||window.event;
+          this.style.cursor = 'move';
+        });
+
+      }
+
+      for(let i = 0; i < img.length; i++) {
+        img[i].style.position = 'absolute';
+      }
+
     }
 
-  });
-
-  container.addEventListener('mousemove', function (e) {
-    e=e||window.event;
-    e.preventDefault();
-
-    if(elem) {
-      elem.style.left = e.pageX - mouseX + 'px';
-      elem.style.top = e.pageY - mouseY + 'px';
-    }
-
-  });
-
-
-  container.addEventListener('mouseup', function (e) {
-    e=e||window.event;
-    elem = null;
-  });
-
-
-
-
-
-
-
-
-
-  function renderAbsoluteImg(img) {
-
-    for(let i = 0; i < img.length; i++) {
-      let image = img[i];
-      let left = image.offsetLeft;
-      let top = image.offsetTop;
-      image.style.left = left + 'px';
-      image.style.top = top + 'px';
-    }
-
-    for(let i = 0; i < img.length; i++) {
-      img[i].style.position = 'absolute';
-    }
 
   }
 
+  dragImages();
 
-}
+});
 
-dragImages();
