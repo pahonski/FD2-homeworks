c3b81838c12c6503f7c2f0c1e8fc3286b7853e91
Pavel <pahonski@gmail.com>
2018-07-24 21:11:14 +0300

    add circles

--- CLOCK_DOM/index.js
+++ CLOCK_DOM/index.js
@@ -11,8 +11,11 @@ function renderClock() {
   var clockCenterX = clock.offsetLeft + clock.offsetWidth/2;
   var clockCenterY = clock.offsetTop + clock.offsetHeight/2;
 
-  var RADIUS = 180;
-  var angle = 150; ///180*Math.PI;
+  var circle = 360;
+  var radius = 170;
+  var angle = 150;
+  var numContainerWidth = 30; //Временная мера
+  var numContainerHeight = 30; //Временная мера
 
   var count = 1;
 
@@ -23,22 +26,28 @@ function renderClock() {
   minute.style.top = clockCenterY - minute.offsetHeight/2 + 'px';
 
   second.style.left = clockCenterX + 'px';
+  second.style.transformOrigin = clockCenterX + 'px';
   second.style.top = clockCenterY - second.offsetHeight/2 + 'px';
 
   while (count <= 12 && angle >= -180) {
     var numeral = document.createElement('div');
-    var numeralCenterX = clockCenterX + RADIUS * Math.sin(angle/180*Math.PI);
-    var numeralCenterY = clockCenterY + RADIUS * Math.cos(angle/180*Math.PI);
+    var numeralCenterX = clockCenterX + radius * Math.sin(angle/180*Math.PI);
+    var numeralCenterY = clockCenterY + radius * Math.cos(angle/180*Math.PI);
     numeral.classList.add('numeral-container');
     numeral.textContent = count;
-    numeral.style.left = Math.round(numeralCenterX-numeral.offsetWidth/2)+'px';
-    numeral.style.top = Math.round(numeralCenterY-numeral.offsetHeight/2)+'px';
+    numeral.style.left = Math.round(numeralCenterX-numContainerWidth/2)+'px';
+    numeral.style.top = Math.round(numeralCenterY-numContainerHeight/2)+'px';
 
     clock.appendChild(numeral);
 
     count++;
     angle -= 30;
   }
+
+  setInterval(function () {
+    second.style.transform = 'rotate(' + (+360/(+60)) + 'deg)';
+
+  }, 1000)
 
 
 
