d3ca90e14610fd3500480d6cec87d170a9f9521a
Pavel <pahonski@gmail.com>
2018-07-26 13:34:26 +0300

    add clock

--- CLOCK_DOM/index.js
+++ CLOCK_DOM/index.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', function () {
-  renderClock();
+  clock();
 });
 
-function renderClock() {
+function clock() {
   var clock = document.querySelector('.clock');
   var hour = document.querySelector('.hour');
   var minute = document.querySelector('.minute');
@@ -19,36 +19,53 @@ function renderClock() {
 
   var count = 1;
 
-  hour.style.left = clockCenterX + 'px';
-  hour.style.top = clockCenterY - hour.offsetHeight/2 + 'px';
+  renderClock();
 
-  minute.style.left = clockCenterX + 'px';
-  minute.style.top = clockCenterY - minute.offsetHeight/2 + 'px';
+  window.setInterval(
+    function(){
+      var d = new Date();
+      clockGo(d);
+      console.log('hi');
+    }
+    , 500);
 
-  second.style.left = clockCenterX + 'px';
-  second.style.transformOrigin = clockCenterX + 'px';
-  second.style.top = clockCenterY - second.offsetHeight/2 + 'px';
+  function renderClock() {
 
-  while (count <= 12 && angle >= -180) {
-    var numeral = document.createElement('div');
-    var numeralCenterX = clockCenterX + radius * Math.sin(angle/180*Math.PI);
-    var numeralCenterY = clockCenterY + radius * Math.cos(angle/180*Math.PI);
-    numeral.classList.add('numeral-container');
-    numeral.textContent = count;
-    numeral.style.left = Math.round(numeralCenterX-numContainerWidth/2)+'px';
-    numeral.style.top = Math.round(numeralCenterY-numContainerHeight/2)+'px';
 
-    clock.appendChild(numeral);
+    hour.style.left = clockCenterX - hour.offsetWidth/2 + 'px';
+    hour.style.top = clockCenterY - hour.offsetHeight + 'px';
 
-    count++;
-    angle -= 30;
+    minute.style.left = clockCenterX - minute.offsetWidth/2 + 'px';
+    minute.style.top = clockCenterY - minute.offsetHeight + 'px';
+
+    second.style.left = clockCenterX - second.offsetWidth/2 + 'px';
+    second.style.top = clockCenterY - second.offsetHeight + 'px';
+    console.log(clockCenterY - second.offsetHeight/2 + 'px');
+
+    while (count <= 12 && angle >= -180) {
+      var numeral = document.createElement('div');
+      var numeralCenterX = clockCenterX + radius * Math.sin(angle/180*Math.PI);
+      var numeralCenterY = clockCenterY + radius * Math.cos(angle/180*Math.PI);
+      numeral.classList.add('numeral-container');
+      numeral.textContent = count;
+      numeral.style.left = Math.round(numeralCenterX-numContainerWidth/2)+'px';
+      numeral.style.top = Math.round(numeralCenterY-numContainerHeight/2)+'px';
+
+      clock.appendChild(numeral);
+
+      count++;
+      angle -= 30;
+    }
   }
 
-  setInterval(function () {
-    second.style.transform = 'rotate(' + (+360/(+60)) + 'deg)';
+  function clockGo(d) {
+    var t_sec = 6*d.getSeconds();  //Определяем угол для секунд
+    var t_min = 6*(d.getMinutes() + (1/60)*d.getSeconds()); //Определяем угол для минут
+    var t_hour = 30*(d.getHours() + (1/60)*d.getMinutes());  //Определяем угол для часов
+    second.style.transform = "rotate(" + t_sec +"deg)";
+    minute.style.transform = "rotate(" + t_min +"deg)";
+    hour.style.transform = "rotate(" + t_hour +"deg)";
 
-  }, 1000)
-
-
-
+  }
 }
+
