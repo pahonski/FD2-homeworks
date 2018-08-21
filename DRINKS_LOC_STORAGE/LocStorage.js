1eb96e41f5f357b3d0ec6089fe30e20d6d8ca593
Pavel <pahonski@gmail.com>
2018-08-21 20:45:31 +0300

    add drink loc storage new

--- DRINKS_LOC_STORAGE/LocStorage.js
+++ DRINKS_LOC_STORAGE/LocStorage.js
@@ -2,10 +2,41 @@ function LocStorage(name) {
   this.locHash = {"data": []};
   let that = this;
 
-  this.addToLocal = function (item, desc) {
-    console.log(this.locHash);
-    this.locHash.data.push({"header": item, "description": desc});
+  this.addValue = function (key, value) {
+    this.locHash.data.push({"header": key, "description": value});
     localStorage.setItem(name, JSON.stringify(this.locHash));
+    console.log(this.locHash);
+    // this.storage[key] = value;
+  };
+
+  this.getValue = function (key) {
+    let result;
+    this.locHash.data.forEach(function (item) {
+      if(item.header === key) {
+        result = item.description;
+      }
+    });
+    return result ? result : undefined;
+  };
+
+  this.deleteValue = function (key) {
+    let result = false;
+    this.locHash.data.forEach(function (item, index) {
+      if (item.header === key) {
+        that.locHash.data.splice(index, 1);
+        result = true;
+      }
+    });
+    localStorage.setItem(name, JSON.stringify(this.locHash));
+    return result;
+  };
+
+  this.getKeys = function () {
+    let keysArray = [];
+    this.locHash.data.forEach(function (item) {
+      keysArray.push(item.header);
+    });
+    return keysArray;
   };
 
   this.deleteFromLocal = function (key) {
--- DRINKS_LOC_STORAGE/index.js
+++ DRINKS_LOC_STORAGE/index.js
@@ -5,21 +5,16 @@ let addButtonFood = document.querySelector('.add-button-food');
 let alcValue = '';
 let foodValue = '';
 
-let drinkStorage = new HashStorage();
-let foodStorage = new HashStorage();
-
-let drinkLocalStorage = new LocStorage("drinks");
-let foodLocalStorage = new LocStorage('food');
+let drinkStorage = new LocStorage("drinks");
+let foodStorage = new LocStorage('food');
 
 if(localStorage.length > 0) {
   if(localStorage.getItem("drinks")) {
-    drinkStorage.storage = JSON.parse(localStorage.getItem("drinks"));
-    drinkLocalStorage.locHash = JSON.parse(localStorage.getItem("drinks"));
+    drinkStorage.locHash = JSON.parse(localStorage.getItem("drinks"));
   }
 
   if(localStorage.getItem("food")) {
-    foodStorage.storage = JSON.parse(localStorage.getItem("food"));
-    foodLocalStorage.locHash = JSON.parse(localStorage.getItem("food"));
+    foodStorage.locHash = JSON.parse(localStorage.getItem("food"));
   }
 }
 
@@ -47,11 +42,10 @@ addButton.addEventListener('click', function () {
   dataValue.reciple = myReciple;
 
   drinkStorage.addValue(drinkName, dataValue);
-  drinkLocalStorage.addToLocal(drinkName, dataValue);
 
 
   console.info('Напиток добавлен!');
-  console.log(drinkStorage.storage);
+  console.log(drinkStorage.locHash);
 });
 
 let getValueButton = document.querySelector('.get-value-button');
@@ -67,7 +61,6 @@ deleteValueButton.addEventListener('click', function () {
   let key = document.querySelector('#delete-value').value.toLowerCase();
 
   console.log(drinkStorage.deleteValue(key));
-  drinkLocalStorage.deleteFromLocal(key);
 
 });
 
@@ -102,7 +95,6 @@ addButtonFood.addEventListener('click', function () {
   dataValue.reciple = myReciple;
 
   foodStorage.addValue(foodName, dataValue);
-  foodLocalStorage.addToLocal(foodName, dataValue);
 
 
   console.info('Блюдо добавлено!');
@@ -121,7 +113,6 @@ deleteValueButtonFood.addEventListener('click', function () {
   let key = document.querySelector('#delete-value-food').value.toLowerCase();
 
   console.log(foodStorage.deleteValue(key));
-  foodLocalStorage.deleteFromLocal(key);
 
 });
 
